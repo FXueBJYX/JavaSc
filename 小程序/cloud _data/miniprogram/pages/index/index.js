27016621ac46c79752bb8c9dfb616caeebe085c8
2063258898@qq.com <2063258898@qq.com>
2020-12-06 14:01:11 +0800

    小程序

--- 小程序/cloud _data/miniprogram/pages/index/index.js
+++ 小程序/cloud _data/miniprogram/pages/index/index.js
@@ -1,4 +1,6 @@
 // pages/index/index.js
+const db = wx.cloud.database()
+
 Page({
 
   /**
@@ -8,8 +10,71 @@ Page({
 
   },
 
-  add(){
+  async add(){
+    try {
+      const res = await db.collection('student').add({
+        data: {
+          name: 'zhaoliu',
+          age: 20,
+          hobby: ['篮球123', '足球', '排球'],
+          obj: {
+            className: 'wechat',
+            content: 'cloud'
+          }
+        }
+      })
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  async delete(){
+    try{
+      const res = await db.collection('student').doc('846bf2595fcc65c600063e2c1ff2b2d1').remove()
+      console.log(res);
+    }catch(err){
+      console.error(err);
+    }
     
+  },
+
+  async update(){
+    try{
+      const res = await db.collection('student').doc('cc9f30fe5fcc64160006cfcc2f7a711f').update({
+      // data 传入需要局部更新的数据
+      data: {
+        // 表示将 done 字段置为 true
+        name: 'zhangsan'
+      }
+    })
+    console.log(res);
+    }catch(err){
+      console.error(err);
+    }
+    
+  },
+  async select(){
+    try{
+      const res =  await db.collection('student').where({
+        name:"zhaoliu"
+    }).get()
+    console.log(res);
+    }catch(err){
+      console.error(err);
+    }
+    
+  },
+  sum(){
+    wx.cloud.callFunction({
+      name: 'sum1',
+      data: {
+        x: 11,
+        y: 6
+      }
+    }).then(res=>{
+      console.log(res)
+    })
   },
 
   /**
